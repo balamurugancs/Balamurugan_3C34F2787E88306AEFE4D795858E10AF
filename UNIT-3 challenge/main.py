62cefef1ef86e89fcc30f82817a953b1d9a53008
balamurugancs <144466188+balamurugancs@users.noreply.github.com>
2023-10-13 22:50:12 -0700

    Add files via upload

---
+++ UNIT-3 challenge/main.py
@@ -0,0 +1,13 @@
+# recursive for factorial of a given number
+def fact_rec(n):
+  if n==0 or n==1:
+     return 1
+  else:
+     return n*fact_rec(n-1)
+
+number = 20
+res = fact_rec(number)
+
+print("The factorial of {} is {}". format(number,res))
+
+
